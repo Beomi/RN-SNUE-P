643a5dd74cc4daaa100daf09335689550c6ac756
beomi <latheledusjp@gmail.com>
2016-10-09 17:28:48 +0900

    update tabbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,24 +5,24 @@ const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>{props.headerText}</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#F8F8F8',
+    backgroundColor: '#CCC',
     justifyContent: 'center',
     alignItems: 'center',
     height: 60,
-    paddingTop: 15,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
+    paddingTop: 20,
+    shadowColor: '#AAA',
+    shadowOffset: { width: 2, height: 4 },
     shadowOpacity: 0.2,
     elevation: 2,
-    position: 'relative'
+    position: 'relative',
   },
   textStyle: {
     fontSize: 20
